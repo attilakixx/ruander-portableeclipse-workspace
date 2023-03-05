e3141fd611e2a1b1c1fdbef9598b1493f4ad95f5
attilakixx <90560109+attilakixx@users.noreply.github.com>
2023-03-05 19:00:03 +0100

    Create F09Tomb20Elem.java

---
+++ HaziFeladat0225/src/F09Tomb20Elem.java
@@ -0,0 +1,9 @@
+
+public class F09Tomb20Elem {
+//Töltsünk fel egy 20 elemű tömböt véletlen számokkal [-60;+60] intervallumból, majd írjuk ki az 
+//	elemeket egymás mellé szóközzel elválasztva! Alatta írjuk ki egy sorban egymás mellé tabulátorral 
+//	elválasztva a negatív számok közül azokat, amelyek kisebbek -20-nál!
+
+	public static void main(String[] args) {
+	}
+}
